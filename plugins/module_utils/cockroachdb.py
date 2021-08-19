2b3841603783410daddbc19b7c7e1a502f58f907
Andrew Klychkov <aklychko@redhat.com>
2021-08-19 13:29:04 +0200

    Initial commit

---
+++ plugins/module_utils/cockroachdb.py
@@ -0,0 +1,138 @@
+# This code is part of Ansible, but is an independent component.
+# This particular file snippet, and this file snippet only, is BSD licensed.
+# Modules you write using this snippet, which is embedded dynamically by Ansible
+# still belong to the author of the module, and may assign their own license
+# to the complete work.
+#
+# Simplified BSD License (see licenses/simplified_bsd.txt or https://opensource.org/licenses/BSD-2-Clause)
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+psycopg2 = None
+try:
+    import psycopg2
+    HAS_PSYCOPG2 = True
+except ImportError:
+    HAS_PSYCOPG2 = False
+
+from ansible.module_utils.basic import missing_required_lib
+from ansible.module_utils._text import to_native
+from ansible.module_utils.six import iteritems
+from distutils.version import LooseVersion
+
+
+def cockroachdb_common_argument_spec():
+    """
+    Return a dictionary with common options.
+
+    The options are used by most of CockroachDB modules.
+    """
+    return dict(
+        login_user=dict(default='root'),
+        login_password=dict(default='', no_log=True),
+        login_host=dict(default='localhost'),
+        login_unix_socket=dict(default=''),
+        login_port=dict(type='int', default=26257),
+        ssl_mode=dict(
+            default='prefer',
+            choices=[
+                'allow',
+                'disable',
+                'prefer',
+                'require',
+                'verify-ca',
+                'verify-full',
+            ],
+        ),
+        ssl_root_cert=dict(type='path'),
+        ssl_cert=dict(type='path'),
+        ssl_key=dict(type='path'),
+    )
+
+
+def ensure_required_libs(module):
+    """Check required libraries.
+
+    Args:
+        module (AnsibleModule) -- object of ansible.module_utils.basic.AnsibleModule class
+    """
+    if not HAS_PSYCOPG2:
+        module.fail_json(msg=missing_required_lib('psycopg2'))
+
+    if module.params.get('ca_cert') and LooseVersion(psycopg2.__version__) < LooseVersion('2.4.3'):
+        module.fail_json(msg='psycopg2 must be at least 2.4.3 in order to use the ca_cert parameter')
+
+
+class CockroachDB():
+    """Class for working with CockroachDB.
+
+    Args:
+        module (AnsibleModule) -- object of ansible.module_utils.basic.AnsibleModule class
+    """
+    def __init__(self, module):
+        self.module = module
+        self.connection = None
+
+    def connect(self, conn_params, autocommit=False, fail_on_conn=True):
+        """Connect to a CockroachDB database.
+
+        Return psycopg2 connection object.
+
+        Args:
+            conn_params (dict) -- dictionary with connection parameters
+
+        Kwargs:
+            autocommit (bool) -- commit automatically (default False)
+            fail_on_conn (bool) -- fail if connection failed or just warn and return None (default True)
+        """
+        ensure_required_libs(self.module)
+
+        try:
+            self.connection = psycopg2.connect(**conn_params)
+            if autocommit:
+                if LooseVersion(psycopg2.__version__) >= LooseVersion('2.4.2'):
+                    self.connection.set_session(autocommit=True)
+                else:
+                    self.connection.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+
+        except Exception as e:
+            if fail_on_conn:
+                self.module.fail_json(msg="unable to connect to database: %s" % to_native(e))
+            else:
+                self.module.warn("CockroachDB server is unavailable: %s" % to_native(e))
+                self.connection = None
+
+        return self.connection
+
+
+def get_conn_params(params_dict):
+    """Get connection parameters from the passed dictionary.
+
+    Return a dictionary with parameters to connect to CockroachDB server.
+
+    Args:
+        params_dict (dict) -- dictionary with variables
+    """
+    # To use defaults values, keyword arguments must be absent, so
+    # check which values are empty and don't include in the return dictionary
+    params_map = {
+        'login_host': 'host',
+        'login_user': 'user',
+        'login_password': 'password',
+        'login_port': 'port',
+        'ssl_mode': 'sslmode',
+        'ssl_root_cert': 'sslrootcert',
+        'ssl_cert': 'sslcert',
+        'ssl_key': 'sslkey',
+    }
+
+    kw = dict((params_map[k], v) for (k, v) in iteritems(params_dict)
+              if k in params_map and v != '' and v is not None)
+
+    # If a login_unix_socket is specified, incorporate it here.
+    is_localhost = "host" not in kw or kw["host"] is None or kw["host"] == "localhost"
+    if is_localhost and params_dict["login_unix_socket"] != "":
+        kw["host"] = params_dict["login_unix_socket"]
+
+    return kw
---
+++ plugins/modules/cockroachdb_query.py
@@ -0,0 +1,155 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+DOCUMENTATION = r''' # '''
+
+EXAMPLES = r''' # '''
+
+RETURN = r''' # '''
+
+import datetime
+import decimal
+
+try:
+    from psycopg2 import ProgrammingError as Psycopg2ProgrammingError
+except ImportError:
+    # it is needed for checking 'no result to fetch' in main(),
+    # psycopg2 availability will be checked by connect_to_db() into
+    # ansible.module_utils.postgres
+    pass
+
+from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils._text import to_native
+
+from ansible_collections.community.cockroachdb.plugins.module_utils.cockroachdb import (
+    cockroachdb_common_argument_spec,
+    CockroachDB,
+    get_conn_params,
+)
+
+TYPES_NEED_TO_CONVERT = (decimal.Decimal, datetime.timedelta)
+
+
+def convert_to_supported(val):
+    """Convert unsupported type to appropriate.
+
+    Args:
+        val (any) -- Any value fetched from database.
+
+    Returns value of appropriate type.
+    """
+    if isinstance(val, decimal.Decimal):
+        return float(val)
+
+    elif isinstance(val, datetime.timedelta):
+        return str(val)
+
+    return val  # By default returns the same value
+
+
+def fetch_from_cursor(cursor):
+    """Fetch rows from cursor handling unsupported types.
+
+    Args:
+        cursor (cursor): Cursor object of a database Python connector.
+
+    Returns query_result list.
+    """
+    query_result = []
+    for row in cursor:
+        # Ansible engine does not support some types like decimals and timedelta.
+        # An explicit conversion is required on the module's side.
+        for i, elem in enumerate(row):
+            if type(elem) in TYPES_NEED_TO_CONVERT:
+                row[i] = convert_to_supported(elem)
+
+        query_result.append(row)
+
+    return query_result
+
+
+def execute(module, conn, cursor, query):
+    """Execute query in CockroachDB database.
+
+    Args:
+        module (AnsibleModule) -- Object of ansible.module_utils.basic.AnsibleModule class.
+        conn (psycopg2 connection) -- Psycopg2 connection object.
+        cursor (cursor): Cursor object of a database Python connector.
+        query (str) -- Query to execute.
+
+    Returns a tuple (
+        statusmessage (str) -- Status message returned by psycopg2, for example, "SELECT 1".
+        rowcount (int) -- Number of rows fetched, for example, 1.
+        query_result (list) -- List that contains lists [[col1_val, col2_val, ...], [...]].
+    )
+    """
+    statusmessage = None
+    rowcount = None
+    query_result = []
+    try:
+        cursor.execute(query)
+        statusmessage = cursor.statusmessage
+        rowcount = cursor.rowcount
+
+        try:
+            query_result = fetch_from_cursor(cursor)
+
+        except Psycopg2ProgrammingError as e:
+            if to_native(e) == 'no results to fetch':
+                pass
+
+        except Exception as e:
+            module.fail_json(msg='Cannot fetch rows from cursor: %s' % to_native(e))
+
+    except Exception as e:
+        module.fail_json(msg='Cannot execute query "%s": %s' % (query, to_native(e)))
+
+    finally:
+        cursor.close()
+        conn.close()
+
+    return statusmessage, rowcount, query_result
+
+
+def main():
+    # Set up arguments
+    argument_spec = cockroachdb_common_argument_spec()
+    argument_spec.update(
+        query=dict(type='str'),
+    )
+
+    # Instantiate an object of module class
+    module = AnsibleModule(
+        argument_spec=argument_spec,
+        supports_check_mode=False,
+    )
+
+    # Assign passed options to variables
+    query = module.params["query"]
+
+    # Connect to DB, get cursor
+    cockroachdb = CockroachDB(module)
+    conn = cockroachdb.connect(conn_params=get_conn_params(module.params))
+    cursor = conn.cursor()
+
+    # Execute query
+    statusmessage, rowcount, query_result = execute(module, conn, cursor, query)
+
+    # Users will get this in JSON output after execution
+    kw = dict(
+        changed=True,
+        statusmessage=statusmessage,
+        rowcount=rowcount,
+        query_result=query_result,
+    )
+
+    module.exit_json(**kw)
+
+
+if __name__ == '__main__':
+    main()
