7659807eb8126be952131a26b328e44648b03d52
Andrew Klychkov <aklychko@redhat.com>
2021-11-09 13:22:50 +0100

    Get server version

--- plugins/modules/cockroachdb_info.py
+++ plugins/modules/cockroachdb_info.py
@@ -26,8 +26,6 @@ extends_documentation_fragment:
 
 notes:
   - Supports C(check_mode).
-
-options:
 '''
 
 EXAMPLES = r'''
@@ -62,13 +60,55 @@ RETURN = r''' # '''
 
 from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils._text import to_native
-from ansible.module_utils.six import iteritems
 
 from ansible_collections.community.cockroachdb.plugins.module_utils.cockroachdb import (
     common_argument_spec,
     CockroachDB,
     get_conn_params,
 )
+
+
+def exec_query(module, cursor, query):
+    try:
+        cursor.execute(query)
+    except Exception as e:
+        module.fail_json('Failed to execute query "%s": %s' % (query, to_native(e)))
+
+    try:
+        res = cursor.fetchall()
+    except Exception as e:
+        module.fail_json('Failed to fetch rows for query "%s" '
+                         'from cursor: %s' % (query, to_native(e)))
+
+    return res
+
+
+def extract_server_ver(ver_str):
+    version_info = {}
+    version_info['raw'] = ver_str
+
+    try:
+        tmp = ver_str.split('.')[:3]
+        version_info['year'] = int(tmp[0][-2:])
+        version_info['release'] = int(tmp[1])
+        version_info['patch'] = int(tmp[2].split(' ')[0])
+    except Exception as e:
+        version_info['error'] = to_native(e)
+        return (version_info, False)
+
+    return (version_info, True)
+
+
+def get_server_version(module, cursor):
+    res = exec_query(module, cursor, 'SELECT VERSION()')
+
+    v_info, ok = extract_server_ver(res[0][0])
+    if not ok:
+        msg = ('Cannot fetch version from '
+               '"%s": %s' % (v_info['raw'], v_info['error']))
+        module.fail_json(msg)
+
+    return v_info
 
 
 def main():
@@ -85,8 +125,18 @@ def main():
     cockroachdb = CockroachDB(module)
 
     conn = cockroachdb.connect(conn_params=get_conn_params(module.params),
-                               autocommit=True, rows_type='dict')
+                               autocommit=True)
     cursor = conn.cursor()
+
+    # Dictionary that will contain server information.
+    # We will return it to users at the end
+    server_info = {
+        'version': {},
+    }
+
+    # Collect info
+    # TODO: implement via loop with filtering
+    server_info['version'] = get_server_version(module, cursor)
 
     # Close cursor and conn
     try:
@@ -95,12 +145,7 @@ def main():
     except Exception:
         pass
 
-    # Users will get this in JSON output after execution
-    kw = dict(
-        changed=False,
-    )
-
-    module.exit_json(**kw)
+    module.exit_json(changed=False, **server_info)
 
 
 if __name__ == '__main__':
